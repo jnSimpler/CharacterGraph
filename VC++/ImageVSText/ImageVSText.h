f38255e9736d29aad4225aaecfc6cb0e7d329f39
semprathlon <semprathlon@163.com>
2015-02-27 21:29:16 +0800

    VC++ ridiculous function No.1 applied

--- VC++/ImageVSText/ImageVSText.h
+++ VC++/ImageVSText/ImageVSText.h
@@ -15,6 +15,7 @@
 #include <vcclr.h>
 #using <mscorlib.dll>
 
+using namespace cli;
 using namespace std;
 using namespace System;
 using namespace System::ComponentModel;
@@ -24,46 +25,488 @@ using namespace System::Data;
 using namespace System::Drawing;
 using namespace System::Drawing::Imaging;
 using namespace System::Drawing::Drawing2D;
+using namespace System::IO;
 
-class ImgData
+namespace ImgDataTech
 {
-private:
-	bool Existing;
-	gcroot<Bitmap ^> img;
-public:
-	void Create(Bitmap^ bitmap)
+	typedef System::Drawing::Rectangle Rectangle;
+
+	string SStringToSTLString(String^ str)
 	{
-		this->Existing = true;
-		img = gcnew Bitmap(bitmap);
+		return (char*)(void*)System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(str);
 	};
-	void Create(String^ FileName)
+
+	class ImageFunctions
 	{
-		this->Existing = true;
-		img = gcnew Bitmap(FileName, true);
-	}
-	ImgData()
+	public:
+		ImageFormat^ GetImageFormat(string extension)
+		{
+			Guid result;
+
+			switch (extension[1])
+			{
+			case 'p':
+				result = ImageFormat::Png->Guid;
+				break;
+			case 'j':
+				result = ImageFormat::Jpeg->Guid;
+				break;
+			case 'g':
+				result = ImageFormat::Gif->Guid;
+				break;
+			case 't':
+				result = ImageFormat::Tiff->Guid;
+				break;
+			default: // bmp, rle, dib
+				result = ImageFormat::Bmp->Guid;
+				break;
+			}
+
+			return gcnew ImageFormat(result);
+		}
+	};
+
+	class Matrices
 	{
-		this->Existing = false;
-	}
-	Bitmap^ GetData()
-	{
-		return this->img;
-	}
-	int GetW()
-	{
-		return this->img->Width;
-	}
-	int GetH()
-	{
-		return this->img->Height;
-	}
-	void Proceed()
+	public:
+		ColorMatrix^ BrightnessContrast(float brightness, float contrast)
+		{
+			float value = (float(0.5) * (float(1.0) - contrast)) + brightness;
+
+			return gcnew ColorMatrix
+			(
+				gcnew array<array<float> ^>
+				{
+					gcnew array<float> {contrast, 0, 0, 0, 0},
+					gcnew array<float> {0, contrast, 0, 0, 0},
+					gcnew array<float> {0, 0, contrast, 0, 0},
+					gcnew array<float> { 0, 0, 0, 1, 0 },
+					gcnew array<float> {value, value, value, 2, 1},
+				}
+			);
+		};
+
+		static ColorMatrix^ Grayscale()
+		{
+			return gcnew ColorMatrix
+				(
+				gcnew array<array<float> ^>
+			{
+				gcnew array<float> {float(0.299), float(0.299), float(0.299), 0, 0},
+					gcnew array<float> {float(0.587), float(0.587), float(0.587), 0, 0},
+					gcnew array<float> {float(0.114), float(0.114), float(0.114), 0, 0},
+					gcnew array<float> {0, 0, 0, 1, 0},
+					gcnew array<float> {0, 0, 0, 0, 1},
+			}
+			);
+		};
+
+		static ColorMatrix^ Identity()
+		{
+			return gcnew ColorMatrix
+				(
+				gcnew array<array<float> ^>
+			{
+				gcnew array<float> {1, 0, 0, 0, 0},
+					gcnew array<float> {0, 1, 0, 0, 0},
+					gcnew array<float> {0, 0, 1, 0, 0},
+					gcnew array<float> {0, 0, 0, 1, 0},
+					gcnew array<float> {0, 0, 0, 0, 1},
+			}
+			);
+		};
+
+		static ColorMatrix^ Multiply(ColorMatrix^ matrix1, ColorMatrix^ matrix2)
+		{
+			if (matrix1 == nullptr || matrix2 == nullptr) return nullptr;
+			ColorMatrix^ result = gcnew ColorMatrix();
+			for (int x = 0; x < 5; x++)
+			{
+				for (int y = 0; y < 5; y++)
+				{
+					result[x, y] = 0;
+
+					for (int z = 0; z < 5; z++)
+					{
+						result[x, y] += matrix1[x, z] * matrix2[z, y];
+					}
+				}
+			}
+			return result;
+		};
+	};
+
+	class ImageToValues
 	{
 
-	}
-	void Delete()
-	{
-		this->Existing = false;
-		delete this->img;
+	public:
+		static byte** Convert(Image^ image, Size^ outputSize, ColorMatrix^ matrix, Rectangle* section)
+		{
+			if (image == nullptr || matrix == nullptr)
+			{
+				return NULL;
+			}
+
+			byte** result;
+
+			try
+			{
+				result = new byte*[outputSize->Height];
+
+				for (int i = 0; i < outputSize->Height; i++)
+				{
+					result[i] = new byte[outputSize->Width];
+				}
+			}
+			finally// (OutOfMemoryException)
+			{
+				//return null;
+			}
+
+			Rectangle targetSize = Rectangle(0, 0, outputSize->Width, outputSize->Height);
+
+			{
+				Bitmap^ resized = gcnew Bitmap(outputSize->Width, outputSize->Height);
+				// draw a resized version onto the new bitmap
+				{
+					Graphics^ g = Graphics::FromImage(resized);
+
+					g->InterpolationMode = InterpolationMode::HighQualityBicubic;
+
+					g->DrawImage(
+						image,
+						targetSize,
+						section->X,
+						section->Y,
+						section->Width,
+						section->Height,
+						GraphicsUnit::Pixel);
+				}
+
+				// copy the resized version onto a new bitmap, applying the matrix
+				{
+					Bitmap^ target = gcnew Bitmap(outputSize->Width, outputSize->Height);
+					// create the image attributes and pass it the matrix
+					{
+						ImageAttributes^ imageAttributes = gcnew ImageAttributes();
+						// merge the passed matrix with the matrix for converting to greyscale
+						ColorMatrix^ matrices = Matrices::Multiply(Matrices::Grayscale(), matrix);
+
+						imageAttributes->SetColorMatrix(matrices);
+
+						{
+							Graphics^ graphics = Graphics::FromImage(target);
+							graphics->DrawImage(
+								resized,
+								targetSize,
+								0,
+								0,
+								outputSize->Width,
+								outputSize->Height,
+								GraphicsUnit::Pixel,
+								imageAttributes);
+						}
+					}
+
+					BitmapData^ data = target->LockBits(targetSize, ImageLockMode::ReadOnly, PixelFormat::Format24bppRgb);
+					int padding = data->Stride - (outputSize->Width * 3);
+
+					{
+						byte* pointer = (byte*)data->Scan0.ToPointer();
+
+						for (int y = 0; y < outputSize->Height; y++)
+						{
+							for (int x = 0; x < outputSize->Width; x++)
+							{
+								result[y][x] = pointer[2];
+
+								pointer += 3;
+							}
+
+							pointer += padding;
+						}
+					}
+
+					target->UnlockBits(data);
+				}
+			}
+
+			return result;
+		}
+		
+		static byte** Convert(Image^ image, Size^ outputSize, ColorMatrix^ matrix)
+		{
+			if (image == nullptr || matrix == nullptr)
+			{
+				return NULL;
+			}
+
+			return Convert(image, outputSize, matrix, new Rectangle(0, 0, image->Width, image->Height));
+		}
+
+		static byte** Convert(Image^ image, Size^ outputSize)
+		{
+			if (image == nullptr)
+			{
+				return NULL;
+			}
+
+			return Convert(image, outputSize, Matrices::Identity());
+		}
 	};
-} IData;
+
+	class ImageToColors
+	{
+
+	public:
+		static Color** Convert(Image^ image, Size* outputSize, Rectangle* section, bool reducingColors)
+		{
+			Color** outputArray;
+
+			// create the resized and cropped image
+			
+			{
+				Bitmap^ resizedImage = gcnew Bitmap(outputSize->Width, outputSize->Height);
+
+				
+				{
+					Graphics^ g = Graphics::FromImage(resizedImage);
+
+					g->InterpolationMode = InterpolationMode::HighQualityBicubic;
+
+					g->DrawImage(
+						image,
+						Rectangle(0, 0, outputSize->Width, outputSize->Height),
+						section->X,
+						section->Y,
+						section->Width,
+						section->Height,
+						GraphicsUnit::Pixel);
+				}
+
+				if (reducingColors)
+				{
+					// File name for the temporary file
+					String^ temporaryFilename = Path::GetTempFileName();
+
+					// Save as a 256 color gif file
+					resizedImage->Save(temporaryFilename, ImageFormat::Gif);
+
+					// load into an image
+					
+					{
+						Bitmap^ gifImage = gcnew Bitmap(Image::FromFile(temporaryFilename));
+						outputArray = BuildOutputArray(outputSize, gifImage);
+					}
+
+					try
+					{
+						// remove the temporary file
+						File::Delete(temporaryFilename);
+					}
+					finally
+					{
+					}
+				}
+				else
+				{
+					// Build the output array from the 24-bit image
+					outputArray = BuildOutputArray(outputSize, resizedImage);
+				}
+			}
+
+			return outputArray;
+		}
+
+		static Color** Convert(Image^ image, Size* outputSize, bool reduceColors)
+		{
+			if (image == nullptr)
+			{
+				return NULL;
+			}
+
+			return Convert(
+				image,
+				outputSize,
+				new Rectangle(0, 0, image->Width, image->Height),
+				reduceColors);
+		}
+
+		static Color** BuildOutputArray(Size* outputSize, Bitmap^ resizedImage)
+		{
+			Color** result;
+			//array<array<Color> ^> ^ result;
+
+			try
+			{
+				/*result = gcnew array<array<Color> ^>(outputSize->Height);
+				for (int i = 0; i < outputSize->Height; i++)
+				{
+					result[i] = gcnew array<Color>(outputSize->Width);
+				}*/
+				result = new Color*[outputSize->Height];
+				result[0] = (Color *)malloc(outputSize->Height*outputSize->Width*sizeof(Color));
+				for (int i = 1; i < outputSize->Height; i++)
+				{
+					result[i] = result[i - 1] + outputSize->Width;
+				}
+			}
+			finally//catch (System.OutOfMemoryException)
+			{
+				//return null;
+			}
+
+			BitmapData^ data = resizedImage->LockBits(Rectangle(0, 0, outputSize->Width, outputSize->Height), ImageLockMode::ReadOnly, PixelFormat::Format24bppRgb);
+			int padding = data->Stride - (outputSize->Width * 3);
+
+			{
+				byte* pointer = (byte*)data->Scan0.ToPointer();
+
+				for (int y = 0; y < outputSize->Height; y++)
+				{
+					for (int x = 0; x < outputSize->Width; x++)
+					{
+						result[y][x] = Color::FromArgb(pointer[2], pointer[1], pointer[0]);
+
+						pointer += 3;
+					}
+
+					pointer += padding;
+				}
+			}
+
+			return result;
+		}
+	};
+	
+	class ConvertImage
+	{
+	};
+
+	class ImgData
+	{
+	private:
+	public:
+		char* word;
+		bool Existing, Finished;
+		gcroot<Bitmap ^> bmp;
+		byte** values;
+		long len;
+		gcroot<Image ^> img;
+	
+		void Create(Image^ img)
+		{
+			this->Existing = 1;
+			this->Finished = 0;
+			this->img = img;
+			this->bmp = gcnew Bitmap(img);
+		};
+		void Create(String^ FName)
+		{
+			this->Existing = 1;
+			this->Finished = 0;
+			this->img = Image::FromFile(FName);
+			this->bmp = gcnew Bitmap(img);
+		};
+		ImgData()
+		{
+			this->Existing = 0;
+			this->Finished = 0;
+			this->len = 0;
+		}
+		Bitmap^ GetBmp()
+		{
+			return this->bmp;
+		}
+		Image^ GetImg()
+		{
+			return this->img;
+		}
+		int GetW()
+		{
+			return this->img->Width;
+		}
+		int GetH()
+		{
+			return this->img->Height;
+		}
+		void Proceed()
+		{
+			this->Finished = 1;
+		}
+		bool isReady()
+		{
+			return this->Existing;
+		}
+		bool isFinished()
+		{
+			return this->Finished;
+		}
+		bool ConvertI2V(string mask)
+		{
+			if (this->GetW() < 1 || this->GetH() < 1 || mask.length() < 1)
+			{
+				return this->Finished = 0;
+			}
+
+			// convert the image into values
+			this->values = ImageToValues::Convert(
+				this->img,
+				gcnew Size(this->GetW(), this->GetH()),
+				Matrices::Identity(),
+				new Rectangle(0, 0, this->GetW(), this->GetH())
+				);
+
+			/*if (!imgData.ValuesCreated)
+			{
+			imgData.textViewer.Clear();
+
+			MessageBox.Show(
+			Resource.GetString("Out of Memory, Could not convert the image"),
+			Resource.GetString("Error"),
+			MessageBoxButtons.OK,
+			MessageBoxIcon.Error);
+
+			return 0;
+			}*/
+
+			this->len = this->GetW()*this->GetH();
+			int maxl = this->len + this->GetH() + 2;
+			this->word = new char[maxl];
+			memset(this->word, 0, maxl);
+			//memcpy(this->word, this->values, len);
+			{
+				const byte index = 255;
+				short interval = (mask.length() < 257) ? mask.length() : 256;
+				byte *h = this->values[0], *r = h + len;
+				char *q = this->word;
+				for (byte *p = h; p != r; p++)
+				{
+					//*(q++) = mask[((*p) >> 8) % interval];
+					*(q++) = mask[((*p)) % interval];
+					if ((p - h + 1) % this->GetW() == 0) *(q++) = '\n';
+				};
+				*(q++) = '\0';
+			}
+
+			return this->Finished = 1;
+		}
+		char* GetWord()
+		{
+			return this->word;
+		}
+		void Delete()
+		{
+			this->Existing = 0;
+			this->Finished = 0;
+			delete this->img;
+			delete this->bmp;
+			free(word);
+		};
+		~ImgData()
+		{
+			this->Delete();
+		}
+	} *IData;
+
+}
--- VC++/ImageVSText/MyForm.h
+++ VC++/ImageVSText/MyForm.h
@@ -2,6 +2,8 @@
 
 #include "ImageVSText.h"
 
+using namespace ImgDataTech;
+
 namespace ImageVSText {
 
 	/// <summary>
@@ -16,11 +18,6 @@ namespace ImageVSText {
 			//
 			//TODO: Add the constructor code here
 			//
-		}
-
-		string SStringToSTLString(String^ str)
-		{
-			return (char*)(void*)System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(str);
 		}
 
 	protected:
@@ -43,6 +40,8 @@ namespace ImageVSText {
 	private: System::Windows::Forms::Button^  ProceedButton;
 	private: System::Windows::Forms::PictureBox^  Box1;
 	private: System::Windows::Forms::PictureBox^  Box2;
+	private: System::Windows::Forms::RichTextBox^  TextBox;
+	private: System::Windows::Forms::TextBox^  maskBox;
 	protected:
 
 	private:
@@ -58,9 +57,12 @@ namespace ImageVSText {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->openFileDialog = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->OpenButton = (gcnew System::Windows::Forms::Button());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->maskBox = (gcnew System::Windows::Forms::TextBox());
+			this->TextBox = (gcnew System::Windows::Forms::RichTextBox());
 			this->Box2 = (gcnew System::Windows::Forms::PictureBox());
 			this->Box1 = (gcnew System::Windows::Forms::PictureBox());
 			this->SaveButton = (gcnew System::Windows::Forms::Button());
@@ -73,6 +75,7 @@ namespace ImageVSText {
 			// openFileDialog
 			// 
 			this->openFileDialog->FileName = L"openFileDialog1";
+			this->openFileDialog->Filter = resources->GetString(L"openFileDialog.Filter");
 			this->openFileDialog->FileOk += gcnew System::ComponentModel::CancelEventHandler(this, &MyForm::openFileDialog1_FileOk);
 			// 
 			// OpenButton
@@ -87,6 +90,8 @@ namespace ImageVSText {
 			// 
 			// panel1
 			// 
+			this->panel1->Controls->Add(this->maskBox);
+			this->panel1->Controls->Add(this->TextBox);
 			this->panel1->Controls->Add(this->Box2);
 			this->panel1->Controls->Add(this->Box1);
 			this->panel1->Controls->Add(this->SaveButton);
@@ -95,12 +100,28 @@ namespace ImageVSText {
 			this->panel1->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->panel1->Location = System::Drawing::Point(0, 0);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(529, 263);
+			this->panel1->Size = System::Drawing::Size(761, 263);
 			this->panel1->TabIndex = 1;
+			// 
+			// maskBox
+			// 
+			this->maskBox->Location = System::Drawing::Point(246, 5);
+			this->maskBox->Name = L"maskBox";
+			this->maskBox->Size = System::Drawing::Size(269, 21);
+			this->maskBox->TabIndex = 5;
+			this->maskBox->Text = L".,`\'\"-*+:;/\\|!@#~";
+			// 
+			// TextBox
+			// 
+			this->TextBox->Location = System::Drawing::Point(246, 32);
+			this->TextBox->Name = L"TextBox";
+			this->TextBox->Size = System::Drawing::Size(269, 219);
+			this->TextBox->TabIndex = 4;
+			this->TextBox->Text = L"";
 			// 
 			// Box2
 			// 
-			this->Box2->Location = System::Drawing::Point(280, 32);
+			this->Box2->Location = System::Drawing::Point(521, 32);
 			this->Box2->Name = L"Box2";
 			this->Box2->Size = System::Drawing::Size(237, 219);
 			this->Box2->TabIndex = 3;
@@ -138,11 +159,15 @@ namespace ImageVSText {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(529, 263);
+			this->ClientSize = System::Drawing::Size(761, 263);
 			this->Controls->Add(this->panel1);
 			this->Name = L"MyForm";
 			this->Text = L"Demo of function";
+			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &MyForm::MyForm_FormClosing);
+			this->FormClosed += gcnew System::Windows::Forms::FormClosedEventHandler(this, &MyForm::MyForm_FormClosed);
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Box2))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Box1))->EndInit();
 			this->ResumeLayout(false);
@@ -154,8 +179,8 @@ namespace ImageVSText {
 	private: System::Void OpenButton_Click(System::Object^  sender, System::EventArgs^  e) {
 				 if (openFileDialog->ShowDialog() == System::Windows::Forms::DialogResult::OK)
 				 {
-					 IData.Create(openFileDialog->FileName);
-					 double r1 = IData.GetW() / double(IData.GetH()), r2 = Box1->Width / double(Box1->Height);
+					 IData->Create(openFileDialog->FileName);
+					 double r1 = IData->GetW() / double(IData->GetH()), r2 = Box1->Width / double(Box1->Height);
 					 int W, H;
 					 if (r1 > r2)
 					 {
@@ -165,12 +190,58 @@ namespace ImageVSText {
 					 {
 						 H = Box1->Height; W = int(H * r1);
 					 }
-					 Bitmap^ image = gcnew Bitmap(IData.GetData(), W, H); //resize image
+					 Bitmap^ image = gcnew Bitmap(IData->GetBmp(), W, H); //resize image
 					 Box1->Image = image;
 				 }
 	}
 private: System::Void ProceedButton_Click(System::Object^  sender, System::EventArgs^  e) {
+			 if (IData->isReady())
+			 {
+				 try
+				 {
+					 if (IData->ConvertI2V(SStringToSTLString(maskBox->Text)))
+					 {
+						 TextBox->Text = gcnew String(IData->GetWord());
+					 }
+					 else
+					 {
+						 TextBox->Text = L"Failed";
+					 }
+				 }
+				 finally
+				 {
 
+				 };
+				 try
+				 {
+					 double r1 = IData->GetW() / double(IData->GetH()), r2 = Box2->Width / double(Box2->Height);
+					 int W, H;
+					 if (r1 > r2)
+					 {
+						 W = Box2->Width; H = int(W / r1);
+					 }
+					 else
+					 {
+						 H = Box2->Height; W = int(H * r1);
+					 }
+
+					 Bitmap^ bmp = gcnew Bitmap(IData->GetImg(), W, H);
+					 Box2->Image = bmp;
+				 }
+				 finally
+				 {
+
+				 };
+			 }
+}
+private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+			 IData = new ImgData();
+}
+private: System::Void MyForm_FormClosing(System::Object^  sender, System::Windows::Forms::FormClosingEventArgs^  e) {
+			 
+}
+private: System::Void MyForm_FormClosed(System::Object^  sender, System::Windows::Forms::FormClosedEventArgs^  e) {
+			 delete IData;
 }
 };
 }
